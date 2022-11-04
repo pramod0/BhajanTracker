93b90068734d47d1887ea5c8c74923f92d4c84fa
Gupta <gupta527@avaya.com>
2022-11-04 21:30:57 +0530

    showing date is selected if bhajan is done on that date

--- lib/bhajanTracker.dart
+++ lib/bhajanTracker.dart
@@ -1,6 +1,5 @@
-//import 'dart:html';
+import 'dart:collection';
 
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -26,154 +25,138 @@ class BhajanTrack extends StatefulWidget {
 
 class _BhajanTrackState extends State<BhajanTrack> {
   final _auth = FirebaseAuth.instance;
+  late String user;
 
   @override
   void initState() {
     super.initState();
-
     getCurrentUser();
   }
 
   void getCurrentUser() async {
-    try {
-      final user = await _auth.currentUser!;
-      if (user != null) {
-        print("Hello ");
-        print(user);
-      }
-    } catch (e) {
-      print(e);
-    }
+      user = (await _auth.currentUser!) as String;
   }
+  
   DateTime _selectedDay = DateTime.utc(
       DateTime.now().year, DateTime.now().month, DateTime.now().day - 1);
   DateTime _focusedDay = DateTime.utc(
       DateTime.now().year, DateTime.now().month, DateTime.now().day - 1);
-  CalendarFormat _calendarFormat = CalendarFormat.week;
+  CalendarFormat _calendarFormat = CalendarFormat.month;
 
-
+  Set<String> bhajanDateSet = HashSet();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: hexToColor("#FFBE68"),
-          title: const Text(
-            "Bhajan",
-            style: TextStyle(color: Colors.black, fontSize: 20.0),
-          ),
-          iconTheme: const IconThemeData(
-            color: Colors.black,
-          ),
-        ),
-        body: StreamBuilder<QuerySnapshot>(
-          stream: _firestore.collection('dailytrack').snapshots(),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return const Center(
-                child: CircularProgressIndicator(
-                  backgroundColor: Colors.lightBlueAccent,
-                ),
-              );
-            }
-            final data = snapshot.data?.docs;
-            for (var maps in data!) {
-              print (maps.get("date"));
-            }
-            return Column(children: [
-              const SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(7),
-                    color: Colors.white,
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.grey,
-                        blurRadius: 1.0, // soften the shadow
-                        spreadRadius: 0.5, //extend the shadow
-                        offset: Offset(
-                          0.0, // Move to right 10  horizontally
-                          1.0, // Move to bottom 10 Vertically
-                        ),
-                      )
-                    ],
+    return SafeArea(
+      child: Scaffold(
+          body: StreamBuilder<QuerySnapshot>(
+            stream: _firestore.collection('dailytrack').snapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(
+                  child: CircularProgressIndicator(
+                    backgroundColor: Colors.lightBlueAccent,
                   ),
+                );
+              }
+              final data = snapshot.data?.docs;
+
+              for (var maps in data!) {
+                if(maps.get("user").toString() == _auth.currentUser?.email){
+                  bhajanDateSet.add(maps.get("date"));
+                }
+              }
+
+              return Column(children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
                   child: Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-                    child: TableCalendar(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(7),
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 1.0, // soften the shadow
+                          spreadRadius: 0.5, //extend the shadow
+                          offset: Offset(
+                            0.0, // Move to right 10  horizontally
+                            1.0, // Move to bottom 10 Vertically
+                          ),
+                        )
+                      ],
+                    ),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+                      child: TableCalendar(
 
 
-                      headerStyle: const HeaderStyle(
-                          headerPadding: EdgeInsets.all(12),
-                          rightChevronVisible: false,
-                          leftChevronVisible: false,
-                          formatButtonVisible: false,
-                          titleTextStyle: TextStyle(
-                              fontWeight: FontWeight.w400,
-                              color: Colors.black,
-                              fontSize: 20)),
-                      firstDay: DateTime.utc(2022, 01, 01),
-                      lastDay: DateTime.utc(DateTime.now().year,
-                          DateTime.now().month, DateTime.now().day),
-                      focusedDay: _selectedDay,
-                      calendarFormat: _calendarFormat,
-                      onFormatChanged: (format) {
-                        setState(() {
-                          _calendarFormat = format;
-                        });
-                      },
-                      selectedDayPredicate: (day) {
-                        return isSameDay(_selectedDay, day);
-                      },
-                      onDaySelected: (selectedDay, focusedDay) {
-                        if (!isSameDay(_selectedDay, selectedDay) &&
-                                selectedDay ==
-                                    DateTime.utc(
-                                        DateTime.now().year,
-                                        DateTime.now().month,
-                                        (DateTime.now().day)) ||
-                            selectedDay ==
-                                DateTime.utc(
-                                    DateTime.now().year,
-                                    DateTime.now().month,
-                                    (DateTime.now().day - 1))) {
-                          setState(
-                            () {
-                              _focusedDay = focusedDay;
-                              _selectedDay = selectedDay;
+                        headerStyle: const HeaderStyle(
+                            headerPadding: EdgeInsets.all(12),
+                            rightChevronVisible: false,
+                            leftChevronVisible: false,
+                            formatButtonVisible: false,
+                            titleTextStyle: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black,
+                                fontSize: 20)),
+                        firstDay: DateTime.utc(2022, 01, 01),
+                        lastDay: DateTime.utc(DateTime.now().year,
+                            DateTime.now().month, DateTime.now().day),
+                        focusedDay: _selectedDay,
+                        calendarFormat: _calendarFormat,
+                        onFormatChanged: (format) {
+                          setState(() {
+                            _calendarFormat = format;
+                          });
+                        },
+                        selectedDayPredicate: (day) {
+                          String date = DateFormat('dd-MM-yyyy').format(day).toString();
+                          if(bhajanDateSet.contains(date)){
 
-                              _firestore.collection('dailytrack').add({
-                                'bhajan': true,
-                                'date':DateFormat('dd-MM-yyyy').format(selectedDay),
-                                'user': _auth.currentUser?.email
-                              });
+                            return true;
+                          }
+                          return false;
+                        },
+                       //  eventLoader: _getEventsForDay,
+                        onDaySelected: (selectedDay, focusedDay) {
+                          if (!isSameDay(_selectedDay, selectedDay) &&
+                                  selectedDay ==
+                                      DateTime.utc(
+                                          DateTime.now().year,
+                                          DateTime.now().month,
+                                          (DateTime.now().day)) ||
+                              selectedDay ==
+                                  DateTime.utc(
+                                      DateTime.now().year,
+                                      DateTime.now().month,
+                                      (DateTime.now().day - 1))) {
+                            setState(
+                              () {
+                                _focusedDay = focusedDay;
+                                _selectedDay = selectedDay;
 
-                            },
-                          );
-                        }
-                      },
+                                _firestore.collection('dailytrack').add({
+                                  'bhajan': true,
+                                  'date':DateFormat('dd-MM-yyyy').format(selectedDay),
+                                  'user': _auth.currentUser?.email
+                                });
+
+                              },
+                            );
+                          }
+                        },
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ]);
-          }
-        ));
+              ]);
+            }
+          )),
+    );
   }
 
-List<Map<String, String>> _getEventsForDay(DateTime day) {
-  List<Map<String, String>> subjectAttendanceMap = [];
-  // widget.scheduleRecords?.map((record) => {
-  //   if(record.date == day){
-  //     // print("PRAMOD "+record.details.toString())
-  //     subjectAttendanceMap.add(record.details)
-  //   }
-  // }).toList();
-
-  return subjectAttendanceMap;
-}
 }
